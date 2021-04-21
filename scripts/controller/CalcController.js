9e93f5973bc24bdbd02ec46842e41ffecb7b3a2c
Larissa Olimpio <lrsolimpio@gmail.com>
2021-04-21 00:24:26 -0300

    alteracoes

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,24 @@
 class CalcController{
 
     constructor(){
-        
+
+        this._displayCalc = '0';
+        this.initialize();
+    }
+
+    initialize(){
+
+    }
+
+    get displayCalc(){
+
+        return this._displayCalc;
+
+    }
+
+    set displayCalc(value){
+
+        this._displayCalc = value;
+
     }
 }
