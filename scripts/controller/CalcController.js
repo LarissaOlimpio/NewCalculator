c14d301be1e3f30f9a7cf8d7bb570e27537e9ce8
Larissa Olimpio <lrsolimpio@gmail.com>
2021-04-24 17:32:48 -0300

    modificacoes

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,7 @@ class CalcController{
 
     constructor(){
 
+        this._operation = [];
         this._displayEl = document.querySelector("#display");
         this.starting();
         this.buttonsEvents();
@@ -23,20 +24,28 @@ class CalcController{
 
     }
 
-clearAll(){
+    clearAll(){
 
-}
+        this._operation = [];//limpando o array
 
-clearEntry(){
+    }
 
+    clearEntry(){
 
-}
+        this._operation.pop();//retirando o último item do array
+    }
 
-setError(){
+    addOperation(value){
 
-this.display = "ERROR"
+        this._operation.push(value);//acrescentando item no array
+        console.log(this._operation);
 
-}
+    }
+    setError(){
+
+        this.display = "ERROR"
+
+    }
 
     execB(value){
 
@@ -66,47 +75,62 @@ this.display = "ERROR"
             case '=':
 
                 break;
-            
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':   
+            case '8':
+            case '9':
+
+                this.addOperation(parseInt(value));
+                
+                break;
+
             default:
                 this.setError
             }
+        console.log(this._operation)
+        }
+
+
+        buttonsEvents(){
+
+            let buttons = document.querySelectorAll(".container > .row > button ");
             
+            
+            buttons.forEach((btn, index)=> {
 
-        
+                this.addEventListenerAll(btn,"click drag", e=>{
 
-    }
-
-
-    buttonsEvents(){
-
-        let buttons = document.querySelectorAll(".container > div > button");
-        
-        buttons.forEach((btn, index)=> {
-
-            this.addEventListenerAll(btn,"click drag", e=>{
-
-                let textB = (btn.className.replace("btn btn-number col-sm","").replace("btn btn-others col-sm",""));
-                console.log(textB)
-                this.execB(textB);
-        })
-
-            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{ //mudando aparência do cursor
-                btn.style.cursor = "pointer";
+                    let textB = (btn.className.replace("btn btn-number col-sm","").replace("btn btn-others col-sm",""));
+                    console.log(textB);
+                    this.execB(textB);
+                    
+                    
+                    
             })
 
-        })
+                this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{ //mudando aparência do cursor
+                    btn.style.cursor = "pointer";
+                })
 
-    }
+            })
 
-    get display(){
+        }
 
-        return this._displayEl.innerHTML;
+        get display(){
 
-    }
+            return this._displayEl.innerHTML;
 
-    set display(value){
+        }
 
-        return this._displayEl.innerHTML = value;
+        set display(value){
 
-    }
+            return this._displayEl.innerHTML = value;
+
+        }
 }
